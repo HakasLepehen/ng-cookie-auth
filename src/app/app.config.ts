898f0ef3d919b13b6ae7c6d3d90c56cb10454b22
petrovpv <petrovpv@competentit.ru>
2025-11-10 14:48:48 +0600

    asdasd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig, inject, PLATFORM_ID,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {HttpClient, provideHttpClient, withInterceptors} from '@angular/common/http';
+import {isPlatformServer} from '@angular/common';
+import {firstValueFrom} from 'rxjs';
+import {apiInterceptor} from './api-interceptor';
+import {API_URL} from './constants';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {provide: API_URL, useValue: 'http://localhost:3000/'},
+    provideAppInitializer(() => {
+      const url = inject(API_URL);
+      const http = inject(HttpClient);
+      const plId = inject(PLATFORM_ID);
+
+      if (isPlatformServer(plId)) return Promise.resolve();
+      return firstValueFrom(http.get(`${url}csrf-token`));
+    }),
+    provideHttpClient(
+      withInterceptors([apiInterceptor])
+    ),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
